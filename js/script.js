faab044f3c74792903049628037f5a54d8e5c3e5
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-03 23:02:04 -0400

    adds notes and working stripscript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 // console.log('connection confirmed');
 document.querySelectorAll('.item').forEach(album => {
-    album.addEventListener('click', getAlbum);
+    album.addEventListener('click', getAlbum)
 })
 
 function getAlbum() {
     //  TEST CONFIGURATION:
-    // console.log('album clicked');
+    console.log('album clicked');
+    let extractedSearchTerms = '';
+    title = this.querySelector('img').src;
+    const url = new URL(title);
+    let space = '+';
+    console.log(title);
+    console.log(url.pathname.split("/")[2].split(".png")[0]);
+    extractedSearchTerms = url.pathname.split("/")[2].split(".png")[0];
+    for (let i = 0; i < extractedSearchTerms.length; i++) {
+        if (extractedSearchTerms.charAt(i) == '-') {
+            console.log('found hyphen');
+            // console.log(extractedSearchTerms.charAt(i));
+            space = extractedSearchTerms.charAt(i);
+            console.log(space);
+            console.log(extractedSearchTerms.charAt(i))
+        }
+    }
+    console.log(extractedSearchTerms);
+    // console.log(typeof title);
+}
+
+function fetchFromSpotify() {
+
 }
---
+++ notes/Readme.md
@@ -0,0 +1,4 @@
+## Project Notes
+
+[parse-url-module](https://dmitripavlutin.com/parse-url-javascript/)
+[split method](https://www.w3schools.com/jsref/jsref_split.asp)
